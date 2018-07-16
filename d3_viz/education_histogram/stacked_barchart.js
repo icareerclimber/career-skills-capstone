faf45d1f26016c15d18ea5aad891f36548baf261
Keri Wheatley <keri.wheatley@civitaslearning.com>
2018-07-16 14:46:17 -0500

    changes for edu viz

--- d3_viz/education_histogram/stacked_barchart.js
+++ d3_viz/education_histogram/stacked_barchart.js
@@ -1,26 +1,37 @@
 // Borrowed from https://bl.ocks.org/mjfoster83/7c9bdfd714ab2f2e39dd5c09057a55a0
 
-// Load unique job titles json data
-d3.json("05_unique_jobs.json", function(error, json) {
+d3.csv("04_unique_jobs.csv", function(error, data) {
     if (error) throw error;
 
     // Populate values in the job title dropdown using data from the json
     var options = jobDrop
         .selectAll('option')
-        .data(json.map( function (d) {return d.cleaned_job_title} )).enter()
+        .data(data.map( function (d) {return d.cleaned_job_title} )).enter()
         .append('option')
         .text(function (d) { return d; });
 
 });
 
-var salaryData = []
+var eduData = []
 // Load the salary json data
-d3.json("05_salary_data_bar_chart.json", function(error, json) {
+d3.csv("04_edu_data_bar_chart.csv", function(error, data) {
     if (error) throw error;
-    salaryData = json.data
+    eduData = data
 
-    updateGraph(salaryData, 'accountant', 'Ohio')
+    // updateGraph(salaryData, 'accountant', 'Ohio')
 });
+
+// Attach the job title dropdown to the div
+var jobDrop = d3.select("#jobDropdown")
+    .append('select')
+    .attr('class','select')
+    .on('change',onChange);
+
+function onChange() {
+    var jobValue = jobDrop.node().value;
+    console.log(jobValue);
+    updateGraph(eduData, jobValue)
+};
 
 // Set the margins
 var margin = {top: 100, right: 100, bottom: 100, left: 100},
@@ -54,95 +65,93 @@ var y = d3.scaleLinear()
 var z = d3.scaleOrdinal()
     .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-// load the csv and create the chart
-d3.csv("age-groups.csv", function(d, i, columns) {
-  for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
-  d.total = t;
-  return d;
-}, function(error, data) {
-  if (error) throw error;
+function updateGraph(data, jobValue) {
 
-  var keys = data.columns.slice(1);
+    var filteredData = data.filter(function(d) {
+        return (d.cleaned_job_title == jobValue) });
 
-  data.sort(function(a, b) { return b.total - a.total; });
-  x.domain(data.map(function(d) { return d.State; }));
-  y.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
-  z.domain(keys);
+    filteredData.sort(function(d) { return d.count_records; });
+    var keys = filteredData.map(function(d) { return d.subject_name });
 
-  g.append("g")
-    .selectAll("g")
-    .data(d3.stack().keys(keys)(data))
-    .enter().append("g")
-      .attr("fill", function(d) { return z(d.key); })
-    .selectAll("rect")
-    .data(function(d) { return d; })
-    .enter().append("rect")
-      .attr("x", function(d) { return x(d.data.State); })
-      .attr("y", function(d) { return y(d[1]); })
-      .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-      .attr("width", x.bandwidth())
-    .on("mouseover", function() { tooltip.style("display", null); })
-    .on("mouseout", function() { tooltip.style("display", "none"); })
-    .on("mousemove", function(d) {
-      console.log(d);
-      var xPosition = d3.mouse(this)[0]+margin.top;
-      var yPosition = d3.mouse(this)[1]+margin.left;
-      tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-      tooltip.select("text").text(d[1]-d[0]);
-    });
+    x.domain([0, d3.max(filteredData, function(d) { return d.count_records; })]);
+    y.domain(filteredData.map(function(d) { return d.final_degree_category; }));
+    z.domain(keys);
 
-  g.append("g")
-      .attr("class", "axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+    g.append("g")
+      .selectAll("g")
+      .data(d3.stack().keys(keys)(filteredData))
+      .enter().append("g")
+        // .attr("fill", function(d) { return z(d.key); })
+      .selectAll("rect")
+      .data(function(d) { return d; })
+      .enter().append("rect")
+        .attr("x", function(d) { return x(d.count_records); })
+        .attr("y", function(d) { return y(d.final_degree_category); })
+        .attr("width", 5)
+        .attr("height", y.bandwidth())
+      // .on("mouseover", function() { tooltip.style("display", null); })
+      // .on("mouseout", function() { tooltip.style("display", "none"); })
+      // .on("mousemove", function(d) {
+      //   console.log(d);
+      //   var xPosition = d3.mouse(this)[0]+margin.top;
+      //   var yPosition = d3.mouse(this)[1]+margin.left;
+      //   tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+      //   tooltip.select("text").text(d.count_records);
+      // })
+;
 
-  g.append("g")
-      .attr("class", "axis")
-      .call(d3.axisLeft(y).ticks(null, "s"))
-    .append("text")
-      .attr("x", 2)
-      .attr("y", y(y.ticks().pop()) + 0.5)
-      .attr("dy", "0.32em")
-      .attr("fill", "#000")
-      .attr("font-weight", "bold")
-      .attr("text-anchor", "start");
+    g.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + width + ")")
+        .call(d3.axisBottom(y));
 
-  var legend = g.append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-      .attr("text-anchor", "end")
-    .selectAll("g")
-    .data(keys.slice().reverse())
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+    g.append("g")
+        .attr("class", "axis")
+        .call(d3.axisLeft(x).ticks(null, "s"))
+      .append("text")
+        .attr("x", 2)
+        .attr("y", x(x.ticks().pop()) + 0.5)
+        .attr("dy", "0.32em")
+        .attr("fill", "#000")
+        .attr("font-weight", "bold")
+        .attr("text-anchor", "start");
 
-  legend.append("rect")
-      .attr("x", width - 19)
-      .attr("width", 19)
-      .attr("height", 19)
-      .attr("fill", z);
+    // var legend = g.append("g")
+    //     .attr("font-family", "sans-serif")
+    //     .attr("font-size", 10)
+    //     .attr("text-anchor", "end")
+    //   .selectAll("g")
+    //   .data(keys.slice().reverse())
+    //   .enter().append("g")
+    //     .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9.5)
-      .attr("dy", "0.32em")
-      .text(function(d) { return d; });
-});
+    // legend.append("rect")
+    //     .attr("x", width - 19)
+    //     .attr("width", 19)
+    //     .attr("height", 19)
+    //     .attr("fill", z);
 
-  // Prep the tooltip bits, initial display is hidden
-  var tooltip = svg.append("g")
-    .attr("class", "tooltip")
-    .style("display", "none");
-      
-  tooltip.append("rect")
-    .attr("width", 60)
-    .attr("height", 20)
-    .attr("fill", "white")
-    .style("opacity", 0.5);
+    // legend.append("text")
+    //     .attr("x", width - 24)
+    //     .attr("y", 9.5)
+    //     .attr("dy", "0.32em")
+    //     .text(function(d) { return d; });
 
-  tooltip.append("text")
-    .attr("x", 30)
-    .attr("dy", "1.2em")
-    .style("text-anchor", "middle")
-    .attr("font-size", "12px")
-    .attr("font-weight", "bold");
+    // Prep the tooltip bits, initial display is hidden
+    var tooltip = svg.append("g")
+      .attr("class", "tooltip")
+      .style("display", "none");
+        
+    tooltip.append("rect")
+      .attr("width", 60)
+      .attr("height", 20)
+      .attr("fill", "white")
+      .style("opacity", 0.5);
+
+    tooltip.append("text")
+      .attr("x", 30)
+      .attr("dy", "1.2em")
+      .style("text-anchor", "middle")
+      .attr("font-size", "12px")
+      .attr("font-weight", "bold");
+}
