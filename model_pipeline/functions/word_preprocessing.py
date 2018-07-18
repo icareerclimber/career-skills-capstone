8b7066c216793e2e132e83b62310b2caa69aec67
Keri Wheatley <keriwheatley@users.noreply.github.com>
2018-07-18 17:26:36 -0500

    changes to add SMOTE

--- model_pipeline/functions/word_preprocessing.py
+++ model_pipeline/functions/word_preprocessing.py
@@ -1,7 +1,7 @@
-import re, unicodedata
-import nltk
 import contractions
-from nltk import word_tokenize
+import nltk
+import re
+import unicodedata
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 
@@ -12,15 +12,19 @@ def preprocess_list(list_of_summaries):
     
     for summary_text in list_of_summaries:
         summary_text = summary_text.lower()
-        summary_text = re.sub(r'[^\w\s]', '', summary_text)
+        summary_text = summary_text.replace('amp;amp;','')
+        summary_text = summary_text.replace('-','')
+        summary_text = re.sub(r'[^A-Za-z0-9&\-]+', ' ', summary_text)
         summary_text = contractions.fix(summary_text)
         
-        summary_tokens = nltk.word_tokenize(summary_text)
+        summary_tokens = summary_text.split(' ')
         summary_tokens = [word for word in summary_tokens if not word in stop_words]
 
         preprocessed_summary_tokens = []
         for token in summary_tokens:
             token = unicodedata.normalize('NFKD', token).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+            if token.isdigit():
+                continue
             if token[-2:] == 'ed' or token == 'wrote':
                 token = lemmatizer.lemmatize(token,'v')
             if token[-1:] == 's':
