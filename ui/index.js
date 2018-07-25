3fb5405a12f3794a83dfd9d98ced3a8229c6adb3
Yuhang Zhao <yuzhao@athenahealth.com>
2018-07-25 13:06:19 -0400

    update ui

--- ui/index.js
+++ ui/index.js
@@ -14,7 +14,7 @@ class SelectDegree extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="col-lg-1">
         <select name="SelectDegree" placeholder="Degree" value={this.state.selectValue} onChange={this.handleDegreeChange}>
           <option defaultValue="Select Degree" disabled>Select Degree</option>
           <option value="Associates">Associates</option>
@@ -41,12 +41,12 @@ class School extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="col-lg-1">
         <input 
           type='text' 
           placeholder="Enter School"
           onChange={this.handleSchoolChange} />
-       </div>
+      </div>
     );
   }
 }
@@ -128,11 +128,14 @@ class RemoveLastEducation extends React.Component {
   }
 }
 
+const EducationStyle = {
+  display: 'flex'
+}
+
 class Education extends React.Component {
   render() {
     return (
-      <div>
-        <h1 className="Education">{this.props.title}</h1>
+      <div className="row" style={EducationStyle}>
         <SelectDegree appHandleDegreeChange={this.props.appHandleDegreeChange} />
         <School appHandleSchoolChange={this.props.appHandleSchoolChange}/>
         <From appHandleFromChange={this.props.appHandleFromChange}/>
@@ -149,7 +152,7 @@ class Summary extends React.Component {
     return (
       <div>
       <h1> Summary of Current Input </h1>
-      <ul className="summary">
+      <ul className="EducationSummary">
         {this.props.summary.Education.map(m => {
             return (
               <li k={m.id}>
@@ -158,6 +161,26 @@ class Summary extends React.Component {
                 </div>
                 <div>
                   {m.School}
+                </div>
+                <div>
+                  {m.From}
+                </div>
+                <div>
+                  {m.To}
+                </div>
+              </li>
+            )
+          })}
+       </ul>
+      <ul className="WorkSummary">
+        {this.props.summary.Work.map(m => {
+            return (
+              <li k={m.id}>
+                <div>
+                  {m.Title}
+                </div>
+                <div>
+                  {m.Company}
                 </div>
                 <div>
                   {m.From}
@@ -191,6 +214,12 @@ const MockSummary = {
     }
   ],
   Work: [
+      {
+        Title: "Software Engineer",
+        Company: "Motorola",
+        From: "2010-03-06",
+        To: "2011-05-09",
+      }
   ]
 }
 
@@ -206,6 +235,10 @@ class App extends React.Component {
       currentSchool:"",
       currentFrom:"",
       currentTo:"",
+      currentSelectedTitle: "",
+      currentCompany: "",
+      currentWorkFrom: "",
+      currentWorkTo: "",
       result:"no response yet"
     }
     this.handleDegreeChange=this.handleDegreeChange.bind(this)
@@ -214,6 +247,12 @@ class App extends React.Component {
     this.handleToChange=this.handleToChange.bind(this)
     this.handleAddEducation=this.handleAddEducation.bind(this)
     this.handleRemoveLastEducation=this.handleRemoveLastEducation.bind(this)
+    this.handleTitleChange=this.handleTitleChange.bind(this)
+    this.handleCompanyChange=this.handleCompanyChange.bind(this)
+    this.handleWorkFromChange=this.handleWorkFromChange.bind(this)
+    this.handleWorkToChange=this.handleWorkToChange.bind(this)
+    this.handleAddWork=this.handleAddWork.bind(this)
+    this.handleRemoveLastWork=this.handleRemoveLastWork.bind(this)
     this.handleSubmit=this.handleSubmit.bind(this)
   }
 
@@ -223,31 +262,36 @@ class App extends React.Component {
     })
   }
 
-  handleSchoolChange(m) {
+  handleTitleChange(m) {
     this.setState({
-      currentSchool: m
+      currentSelectedTitle: m
+    })
+  }
+  handleCompanyChange(m) {
+    this.setState({
+      currentCompany: m
     })
   }
 
-  handleFromChange(m) {
+  handleWorkFromChange(m) {
     this.setState({
-      currentFrom: m
+      currentWorkFrom: m
     })
   }
 
-  handleToChange(m) {
+  handleWorkToChange(m) {
     this.setState({
-      currentTo: m
+      currentWorkTo: m
     })
   }
 
-  handleAddEducation() {
+  handleAddWork() {
     const sum = this.state.summary
-    sum.Education.push({
-      Degree: this.state.currentSelectDegree,
-      School: this.state.currentSchool,
-      From: this.state.currentFrom,
-      To: this.state.currentTo
+    sum.Work.push({
+      Title: this.state.currentSelectTitle,
+      Company: this.state.currentCompany,
+      From: this.state.currentWorkFrom,
+      To: this.state.currentWorkTo
     })
     this.setState({
       summary: sum
@@ -256,7 +300,11 @@ class App extends React.Component {
       currentSelectDegree: "",
       currentSchool: "",
       currentFrom: "",
-      currentTo: ""
+      currentTo: "",
+      currentSelectTitle: "",
+      currentCompany: "",
+      currentWorkFrom: "",
+      currentWorkTo: ""
     })
   }
 
@@ -273,18 +321,34 @@ class App extends React.Component {
     this.setState({summary: sum })
   }
 
+  handleRemoveLastWork() {
+    const sum = this.state.summary
+    const last = sum.Work[sum.Work.length - 1]
+    this.setState({
+      currentSelectTitle: last.Title,
+      currentCompany: last.Company,
+      currentWorkFrom: last.From,
+      currentWorkTo: last.to
+    })
+    sum.Work.splice(-1,1)
+    this.setState({summary: sum })
+  }
+
   handleSubmit() {
-    axios.get('http://localhost:8000/mockResponse.html')
-         .then(response => this.setState({result: response.data})) 
-         //.then(response => console.log(response)) 
+    axios.get('http://localhost:5000/MockResponse', {
+            headers: { 'crossOrigin': true },
+          }).then(response => this.setState({result: response.data})) 
+          //.then(response => console.log(response)) 
   }
   
   render() {
     return (
       <div className="app">
         <Summary summary={this.state.summary} testMessage={this.state.currentSelectedDegree +" " + this.state.currentSchool + " " + this.state.currentFrom + " " + this.state.currentTo}/>
+        <h1>Education</h1>
         <Education title="Education" appHandleDegreeChange={this.handleDegreeChange} appHandleSchoolChange={this.handleSchoolChange} appHandleFromChange={this.handleFromChange} appHandleToChange={this.handleToChange} appHandleAddEducation={this.handleAddEducation} appHandleRemoveLastEducation={this.handleRemoveLastEducation} currentSummary = {this.state.summary.Education}/>
-        <button type="button" onClick={this.handleSubmit}> Submit Education </button>
+        <Work title="Work" appHandleTitleChange={this.handleTitleChange} appHandleCompanyChange={this.handleCompanyChange} appHandleWorkFromChange={this.handleWorkFromChange} appHandleWorkToChange={this.handleWorkToChange} appHandleAddWork={this.handleAddWork} appHandleRemoveLastWork={this.handleRemoveLastWork} currentSummary = {this.state.summary.Education}/>
+        <button type="button" onClick={this.handleSubmit}> Submit Resume </button>
         <p>{this.state.result}</p>
       </div>
     )
@@ -296,3 +360,4 @@ ReactDOM.render(
     <App />, 
     document.getElementById("root")
 );
+
